26d7e9952c7d54e5f7fc822717363362e012fd65
Ilyas <31530783+Pilot669@users.noreply.github.com>
2017-09-10 20:04:13 +0300

    урок

--- src/com/company/HomeCat.java
+++ src/com/company/HomeCat.java
@@ -1,4 +1,14 @@
 package com.company;
 
 class HomeCat extends Pet {
+    @Override
+    public void sayMyName() {
+
+        System.out.println("Мяу, мяу");
+    }
+
+    void sayMyweight(){
+      System.out.println(weight + name);
+    }
 }
+
--- src/com/company/Pet.java
+++ src/com/company/Pet.java
@@ -4,6 +4,7 @@ public class Pet {
 
     String name;
     float weight;
+    private long id;
 
     public void sayMyName() {
         System.out.println("My name is: " + name);
